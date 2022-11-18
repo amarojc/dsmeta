6224e9bd0f2e57b67839ce458475bc628b9877a6
amarojc <inf.amaro.jc@gmail.com>
2022-11-17 23:53:10 -0300

    Repository findSales criado

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmeta.repositories;
 
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +16,9 @@ public interface SaleRepository extends JpaRepository<Sale, Long>{
 	@Query("FROM Sale s " + 
 			"WHERE LOWER (s.sellerName) LIKE %:searchSeller% ")
 	Page<Sale> searchSeller(@Param("searchSeller") String seller, Pageable pageable);
+	
+	@Query("SELECT obj FROM Sale obj " +
+		   " WHERE obj.date BETWEEN :minDate AND :maxDate ORDER BY obj.amount DESC ")
+	Page<Sale> findSales(LocalDate minDate, LocalDate maxDate, Pageable pageable);
 
 }
