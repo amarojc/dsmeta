30b8ac6fda28cd9c915cc4154413e339ecbc526e
amarojc <inf.amaro.jc@gmail.com>
2022-11-17 22:14:12 -0300

    Repository searchSeller criado

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,18 @@
 package com.devsuperior.dsmeta.repositories;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devsuperior.dsmeta.domain.entity.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	@Query("FROM Sale s " + 
+			"WHERE LOWER (s.sellerName) LIKE %:searchSeller% ")
+	Page<Sale> searchSeller(@Param("searchSeller") String seller, Pageable pageable);
 
 }
