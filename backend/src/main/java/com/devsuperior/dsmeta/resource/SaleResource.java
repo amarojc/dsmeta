910cb1b551efece6cb130485ca9e06c833fe6125
amarojc <inf.amaro.jc@gmail.com>
2022-11-19 00:01:03 -0300

    Endpoint Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/resource/SaleResource.java
+++ backend/src/main/java/com/devsuperior/dsmeta/resource/SaleResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.dsmeta.domain.dto.SaleDTO;
 import com.devsuperior.dsmeta.domain.entity.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping("/sale")
@@ -19,6 +21,10 @@ public class SaleResource {
 
 	@Autowired
 	private SaleService saleService;
+
+	@Autowired
+	private SmsService smsService;
+	
 	
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findSales(
@@ -56,5 +62,10 @@ public class SaleResource {
 		Page<SaleDTO> salesDTO = sales.map(obj -> new SaleDTO(obj));
 		
 		return ResponseEntity.ok().body(salesDTO); 
+	}
+	
+	@GetMapping("/sms/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
